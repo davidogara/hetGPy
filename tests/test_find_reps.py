0362076e3275b54da3705e7c504b3a7cc5584fd1
David O'Gara <david.ogara@wustl.edu>
2023-11-21 16:17:34 -0600

    move test csvs to tests/data

--- tests/test_find_reps.py
+++ tests/test_find_reps.py
@@ -5,23 +5,23 @@ import pandas as pd
 def test_find_reps_on_mcycle():
 
     model = hetGP.hetGP()
-    mcycle = pd.read_csv('tests/mcycle.csv',index_col=False)
+    mcycle = pd.read_csv('tests/data/mcycle.csv',index_col=False)
     X = mcycle['times'].values
     Z = mcycle['accel'].values
     test = model.find_reps(X,Z,rescale=False, return_Zlist=True,normalize=False,
                            inputBounds=None)
     # get data from R output
-    X0 = pd.read_csv('tests/X0_no_normal_no_rescale.csv')['V1'].values
-    Z0 = pd.read_csv('tests/Z0_no_normal_no_rescale.csv')['.'].values
-    Z  = pd.read_csv('tests/Z_no_normal_no_rescale.csv')['.'].values
-    mult = pd.read_csv('tests/mult_no_normal_no_rescale.csv')['.'].values
+    X0   = pd.read_csv('tests/data/X0_no_normal_no_rescale.csv')['V1'].values
+    Z0   = pd.read_csv('tests/data/Z0_no_normal_no_rescale.csv')['.'].values
+    Z    = pd.read_csv('tests/data/Z_no_normal_no_rescale.csv')['.'].values
+    mult = pd.read_csv('tests/data/mult_no_normal_no_rescale.csv')['.'].values
     assert np.allclose(X0,test['X0'])
     assert np.allclose(Z0,test['Z0'])
     assert np.allclose(Z,test['Z'])
     assert np.allclose(mult,test['mult'])
 
     
-    Zlist = pd.read_csv('tests/Zlist_no_normal_no_rescale.csv')['.'].str.split('|').values
+    Zlist = pd.read_csv('tests/data/Zlist_no_normal_no_rescale.csv')['.'].str.split('|').values
     # Zlist has a funky structure, so need to go row by row
     checkList = np.full(fill_value=False, dtype=bool,shape=len(Zlist))
     for zz in range(len(Zlist)):
