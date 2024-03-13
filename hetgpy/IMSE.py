d76336247968353ee87a78cb2799ba0829f83b98
David O'Gara <david.ogara@wustl.edu>
2024-03-13 09:44:19 -0500

    First pass at IMPSE_search

--- hetgpy/IMSE.py
+++ hetgpy/IMSE.py
@@ -4,8 +4,14 @@ from hetgpy.src import EMSE
 from hetgpy import hetGP, homGP
 from hetgpy.covariance_functions import cov_gen
 from scipy.linalg.lapack import dtrtri
-
+from joblib import Parallel, delayed
+from hetgpy.covariance_functions import euclidean_dist
+from hetgpy.utils import duplicated
+from scipy.spatial.distance import pdist
+from scipy.stats.qmc import LatinHypercube
+from scipy.optimize import minimize
 TYPE = type
+
 def IMSPE(model, theta = None, Lambda = None, mult = None, covtype = None, nu= None, eps = np.sqrt(np.finfo(float).eps)):
     '''
     IMSPE of a given design.
@@ -241,3 +247,157 @@ def deriv_crit_IMSPE(x, model, Wijs = None):
     
     tmp[m] <- 2 * (c1_v.T @ g) + c2(x = x[:,m], sigma = model.theta[m], w = W11, type = model.covtype) / v + ((v2 * g + 2 * v * h).T @ Wig) + 2 * (h.T @ W1) + v1 * W11
     return -tmp
+
+
+
+def phiP(design,p=50):
+    '''
+    Implementation of `phiP.R` from `DiceDesign` (necessary for maximinSA_LHS from DiceDesign which is used by IMSPE_search)
+   
+    From DiceDesign:
+    Compute the phiP criterion (Lp norm of the sum of the inverses of the design inter-point distances)
+    Reference: Pronzato, L. and Muller, W.,2012, Design of computer experiments: space filling and beyond, Statistics and Computing, 22:681-701.
+    A higher phiP corresponds to a more regular scaterring of design points
+   
+    Arguments
+    ---------
+    design : nd_arraylike
+        design for a computer experiment
+    p    : int
+        the "p" in the Lp norm which is taken (default=50) 
+    
+    Returns
+    -------
+    fi_p : np.float
+        the phiP criterion
+    '''
+    D = pdist(design)
+    D = D**(-p)
+    fi_p = np.sum(D)**(1/p)
+    return fi_p
+
+
+def IMSPE_search(model, replicate = False, Xcand = None, 
+                        control = dict(tol_dist = 1e-6, tol_diff = 1e-6, multi_start = 20,
+                                       maxit = 100, maximin = True, Xstart = None), Wijs = None, seed = None,
+                        ncores = 1):
+  # Only search on existing designs
+    if(replicate):
+        ## Discrete optimization
+        inputs = []
+        for i in range(model.X0.shape[0]):
+            kw = dict(x = None, model = model, Wijs = Wijs)
+            kw['id'] = i
+            inputs.append(kw)
+            res = Parallel(n_jobs=ncores)(
+            delayed(crit_IMSPE)(**kw) 
+                for kw in inputs
+            )
+        return dict(par = model.X0[[np.argmin(res)],:], 
+        value = min(res), new = False, id = np.argmin(res)
+        )
+    if control is None:
+        control = dict(multi_start = 20, maxit = 100)
+  
+    if control.get('multi_start') is None:
+        control['multi_start'] = 20
+  
+    if control.get('maxit') is None:
+        control['maxit'] = 100
+  
+    if control.get('maximin') is None:
+        control['maximin'] = True
+  
+    if control.get('tol_dist') is None: control['tol_dist'] = 1e-6
+    if control.get('tol_diff') is None: control['tol_diff'] = 1e-6
+    
+    d = model.X0.shape[1]
+  
+    ## Precalculations
+    if Wijs is None: Wijs = Wij(mu1 = model.X0, theta = model.theta, type = model.covtype)
+    
+
+    ## Optimization
+    if Xcand is None:
+        ## Continuous optimization
+        if control.get('Xstart') is not None:
+            Xstart = control['Xstart']
+        else:
+            if seed is None: 
+                seed = np.random.default_rng().choice(2**15) ## To be changed?
+            if control['maximin']:
+                if(d == 1):
+                # perturbed 1d equidistant points
+                    Xstart = (np.linspace(1/2, control['multi_start'] -1/2, control['multi_start']) + np.random.default_rng(seed).uniform(size=control['multi_start'], low = -1/2, high = 1/2)).reshape(-1,1)/control['multi_start']
+                else:
+                    sampler = LatinHypercube(d=d,seed=seed)
+                    Xstart = maximinSA_LHS(sampler(n=control['multi_start']))['design']
+                
+            else:
+                sampler = LatinHypercube(d=d,seed=seed)
+                Xstart = sampler(n=control['multi_start'])
+            
+        res = dict(par = np.nan, value = np.inf, new = np.nan)
+        
+        def local_opt_fun(i):
+            out = minimize(
+            x0  = Xstart[[i],:],
+            fun = crit_IMSPE,
+            jac = deriv_crit_IMSPE,
+            args = (model, i, Wijs),
+            method = "L-BFGS-B", 
+            bounds = [(0,1) for _ in range(d)],
+            options=dict(maxiter=control['maxit'], #,
+                                ftol = control.get('factr',10) * np.finfo(float).eps,#,
+                                gtol = control.get('pgtol',0) # should map to pgtol
+                                )
+            )
+            return(out)
+        
+        all_res = Parallel(n_jobs=ncores)(delayed(local_opt_fun))(i for i in np.arnage(Xstart.shape[0]))
+        all_res_values = [res.value for res in all_res]
+        res_min = np.argmin(all_res_values)
+        par = all_res[res_min]['x']
+        par[par<0] = 0
+        par[par>1] = 1
+        res = dict(par = par, 
+                    value = all_res[res_min], new = True, id = None)
+        
+        
+        if control['tol_dist'] > 0 and control['tol_diff'] > 0:
+            ## Check if new design is not to close to existing design
+            dists = np.sqrt(euclidean_dist(res['par'], model.X0))
+            if np.min(dists) < control['tol_dist']:
+                argmin = np.unravel_index(np.argmin(dists, axis=None), dists.shape)[0]
+                res = dict(par = model.X0[[argmin],:],
+                            value = crit_IMSPE(x = model.X0[[argmin],:], model = model, id = argmin, Wijs = Wijs),
+                            new = False, id = argmin)
+            else:
+                ## Check if IMSPE difference between replication and new design is significative
+                id_closest = np.unravel_index(np.argmin(dists, axis=None), dists.shape)[0] # closest point to new design
+                imspe_rep = crit_IMSPE(model = model, id = id_closest, Wijs = Wijs)
+                if (imspe_rep - res['value'])/res['value'] < control['tol_diff']:
+                    res = dict(par = model.X0[[id_closest],:],
+                            value = imspe_rep,
+                            new = False, id = id_closest)
+        return res
+    
+    else:
+        ## Discrete optimization
+        def crit_IMSPE_mcl(i, model, Wijs, Xcand): 
+            return crit_IMSPE(x = Xcand[[i],:], model = model, Wijs = Wijs)
+        
+        inputs = []
+        for i in range(Xcand.shape[0]):
+            kw = {'model':model,'Wijs':Wijs,'Xcand':Xcand}
+            kw[i] = i
+            inputs.append(kw)
+        res = res = Parallel(n_jobs=ncores)(
+            delayed(crit_IMSPE_mcl)(**kw) 
+                for kw in inputs
+            )
+        
+        tmp = (duplicated(np.vstack(model.X0, Xcand[[np.argmin(res)],:]), fromLast = True))
+        if len(tmp) > 0: 
+            return(dict(par = Xcand[[np.argmin(res)],:,], value = min(res), new = False, id = tmp))
+        return(dict(par = Xcand[[np.argmin(res)],:], value = min(res), new = True, id = None))
