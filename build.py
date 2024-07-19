d14f4b67fba117c09418b183763747a03fd011e1
David O'Gara <david.ogara@wustl.edu>
2024-07-19 13:14:00 -0500

    remove gauss from build.py

--- build.py
+++ build.py
@@ -6,10 +6,6 @@ from setuptools_cpp import CMakeExtension, ExtensionBuilder, Pybind11Extension
 
 ext_modules = [
     Pybind11Extension(
-        "hetgpy.gauss", ["hetgpy/src/gauss.cpp"], 
-        include_dirs=["eigen/"]
-    ),
-    Pybind11Extension(
         "hetgpy.matern", ["hetgpy/src/matern.cpp"], 
         include_dirs=["eigen/"]
     ),
