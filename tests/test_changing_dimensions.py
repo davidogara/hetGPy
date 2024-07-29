f270d21568bcd0fa3811f69087e9b3a22283de5a
David O'Gara <david.ogara@wustl.edu>
2024-07-29 16:19:37 -0500

    resolve bug when changing parameter input size

---
+++ tests/test_changing_dimensions.py
@@ -0,0 +1,43 @@
+# test multiple inputs over repeated experiments
+import sys
+sys.path.append('../')
+sys.path.append('./')
+import numpy as np
+rand = np.random.default_rng(42)
+from hetgpy import hetGP, homGP
+
+def test_hom():
+    n = 50
+    p = 3
+    X = rand.integers(low=1,high=50,size=(n,p))
+    Z = np.sin(np.arange(len(X)))
+    model = homGP()
+    model.mle(X,Z,covtype='Gaussian')
+
+    model2 = homGP()
+    extra = rand.integers(low=1,high=10,size=n).reshape(-1,1)
+    X2 = np.hstack([X, extra])
+    model2.mle(X2,Z,covtype='Gaussian',init={},known={},settings={})
+
+def test_het():
+    n = 50
+    p = 3
+    X = rand.integers(low=1,high=50,size=(n,p))
+    Z = np.sin(np.arange(len(X)))
+    model = hetGP()
+    model.mle(X,Z,covtype='Gaussian')
+
+    model2 = hetGP()
+    extra = rand.integers(low=1,high=10,size=n).reshape(-1,1)
+    X2 = np.hstack([X, extra])
+    model2.mle(X2,
+               Z,
+               covtype='Gaussian',
+               lower= [0.1 for i in range(X2.shape[1])],
+               upper= [10 for i in range(X2.shape[1])],
+               init={},known={},
+               settings={})
+
+if __name__ == "__main__":
+    test_hom()
+    test_het()
