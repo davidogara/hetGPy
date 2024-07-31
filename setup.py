bc220a3a0c0291410971f227a31a17b783cce364
David O'Gara <david.ogara@wustl.edu>
2024-07-31 11:14:49 -0500

    try switching to setup.py and build

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+from typing import Any, Dict
+from setuptools_cpp import CMakeExtension, ExtensionBuilder, Pybind11Extension
 
 packages = \
 ['hetgpy']
@@ -32,7 +34,31 @@ setup_kwargs = {
     'install_requires': install_requires,
     'python_requires': '>=3.10',
 }
-from build_hetgpy import *
+
+ext_modules = [
+    Pybind11Extension(
+        "hetgpy.matern", ["hetgpy/src/matern.cpp"], 
+        include_dirs=["eigen/"]
+    ),
+    Pybind11Extension(
+        "hetgpy.qEI", ["hetgpy/src/qEI.cpp"], 
+        include_dirs=["eigen/"]
+    ),
+    Pybind11Extension(
+        "hetgpy.EMSE", ["hetgpy/src/EMSE.cpp"], 
+        include_dirs=["eigen/"]
+    )
+]
+
+
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmdclass": dict(build_ext=ExtensionBuilder),
+            "zip_safe": False,
+        }
+    )
 build(setup_kwargs)
 
 setup(**setup_kwargs)
