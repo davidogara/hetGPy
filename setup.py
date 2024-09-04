419d45998e9fad840c97c11389b9ac1a4bbc0304
David O'Gara <david.ogara@wustl.edu>
2024-09-04 10:28:45 -0500

    add functionality to dowload eigen automatically

--- setup.py
+++ setup.py
@@ -3,9 +3,38 @@ from setuptools import setup
 from typing import Any, Dict
 from setuptools_cpp import CMakeExtension, ExtensionBuilder, Pybind11Extension
 
+import requests
+import os
+import shutil
+
+
+output = 'eigen'
+os.makedirs(output,exist_ok=True)
+
+url = r'https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz'
+zipname = url.split('/')[-1]
+zippath = os.path.join(output,zipname)
+
+# if already downloaded, skip
+if not os.path.exists(zippath):
+    
+    # download
+    r = requests.get(url)
+    with open(zippath, 'wb') as f:
+        f.write(r.content)
+else:
+    print(f'{zippath} exists, skipping download')
+
+shutil.unpack_archive(zippath,output)
+for item in os.listdir('eigen/eigen-3.4.0'):
+    src = 'eigen/eigen-3.4.0/' + item
+    dst = 'eigen'
+    if not os.path.exists(src):
+        shutil.move(src=src,dst=dst)
+shutil.rmtree('eigen/eigen-3.4.0')
+
+
 setup_kwargs = {}
-
-
 ext_modules = [
     Pybind11Extension(
         "hetgpy.matern", ["hetgpy/src/matern.cpp"], 
