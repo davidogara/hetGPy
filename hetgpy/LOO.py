dca1886ea510b48de93851c856fbde4e3f2f0cad
David O'Gara <david.ogara@wustl.edu>
2024-09-12 14:29:38 -0500

    add LOO docstring

--- hetgpy/LOO.py
+++ hetgpy/LOO.py
@@ -2,61 +2,57 @@ import numpy as np
 from hetgpy import hetGP, homGP
 
 def LOO_preds(model, ids = None):
-    '''
-    #' Provide leave one out predictions, e.g., for model testing and diagnostics. 
-    #' This is used in the method plot available on GP and TP models.
-    #' @title Leave one out predictions
-    #' @param model \code{homGP} or \code{hetGP} model, TP version is not considered at this point
-    #' @param ids vector of indices of the unique design point considered (default to all)
-    #' @return list with mean and variance predictions at x_i assuming this point has not been evaluated
-    #' @export
-    #' @note For TP models, \code{psi} is considered fixed.
-    #' @references
-    #' O. Dubrule (1983), Cross validation of Kriging in a unique neighborhood, Mathematical Geology 15, 687--699. \cr \cr
-    #' 
-    #' F. Bachoc (2013), Cross Validation and Maximum Likelihood estimations of hyper-parameters of Gaussian processes 
-    #' with model misspecification, Computational Statistics & Data Analysis, 55--69.
-    #' 
-    #' @examples
-    #' set.seed(32)
-    #' ## motorcycle data
-    #' library(MASS)
-    #' X <- matrix(mcycle$times, ncol = 1)
-    #' Z <- mcycle$accel
-    #' nvar <- 1
-    ## ' plot(X, Z, ylim = c(-160, 90), ylab = 'acceleration', xlab = "time")
-    #'
-    #' ## Model fitting
-    #' model <- mleHomGP(X = X, Z = Z, lower = rep(0.1, nvar), upper = rep(10, nvar),
-    #'                   covtype = "Matern5_2", known = list(beta0 = 0))
-    #' LOO_p <- LOO_preds(model)
-    #'  
-    #' # model minus observation(s) at x_i
-    #' d_mot <- find_reps(X, Z)
-    #' 
-    #' LOO_ref <- matrix(NA, nrow(d_mot$X0), 2)
-    #' for(i in 1:nrow(d_mot$X0)){
-    #'  model_i <- mleHomGP(X = list(X0 = d_mot$X0[-i,, drop = FALSE], Z0 = d_mot$Z0[-i],
-    #'                      mult = d_mot$mult[-i]), Z = unlist(d_mot$Zlist[-i]),
-    #'                      lower = rep(0.1, nvar), upper = rep(50, nvar), covtype = "Matern5_2",
-    #'                      known = list(theta = model$theta, k_theta_g = model$k_theta_g, g = model$g,
-    #'                                   beta0 = 0))
-    #'  model_i$nu_hat <- model$nu_hat
-    ## ' # For hetGP, need to use the same Lambdas to get the same results  
-    ## '  model_i$Lambda <- model$Lambda[-i] 
-    ## '  model_i <- strip(model_i)
-    ## '  model_i <- rebuild(model_i)
-    #'  p_i <- predict(model_i, d_mot$X0[i,,drop = FALSE])
-    #'  LOO_ref[i,] <- c(p_i$mean, p_i$sd2)
-    #' }
-    #' 
-    #' # Compare results
-    #' 
-    #' range(LOO_ref[,1] - LOO_p$mean)
-    #' range(LOO_ref[,2] - LOO_p$sd2)
-    #' 
-    #' # Use of LOO for diagnostics
-    #' plot(model)
+    r'''
+    Provide leave one out predictions, e.g., for model testing and diagnostics.  This is used in the method plot available on GP and TP models.
+    
+    Parameters
+    ----------
+    model: hetgpy.homGP.homGP or hetgpy.hetGP.hetGP
+        hetGPy model to evaluate. TPs not considered at this point.
+    ids: vector of indices of the unique design point considered (defaults to all) 
+    Returns
+    -------
+    dict with mean and variance predictions at x_i assuming this point has not been evaluated
+    
+    Notes
+    -----
+    For TP models, `psi` is considered fixed
+
+    References
+    ----------
+    O. Dubrule (1983), Cross validation of Kriging in a unique neighborhood, Mathematical Geology 15, 687--699. \cr \cr
+    F. Bachoc (2013), Cross Validation and Maximum Likelihood estimations of hyper-parameters of Gaussian processes 
+    
+    Examples
+    --------
+    >>> from hetgpy import homGP
+    >>> from hetgpy.LOO import LOO_preds
+    >>> from hetgpy.find_reps import find_reps
+    >>> from hetgpy.example_data import mcycle
+    >>> import numpy as np
+    >>> m = mcycle()
+    >>> X, Z = m['times'], m['accel']
+    >>> model = homGP()
+    >>> model.mle(X = X, Z = Z, lower = [0.1], upper = [10],
+                    covtype = "Matern5_2", known = dict(beta0 = 0))
+    >>> LOO_p = LOO_preds(model)
+    >>> d = find_reps(X,Z)
+    >>> LOO_ref = np.zeros(shape=(d['X0'].shape[0],2))
+    >>> rows = np.arange(d['X0'].shape[0])
+    >>> for i in rows:
+            idxs = np.delete(rows,i)
+            model_i = homGP()
+            model_i.mle(X = dict(X0 = d['X0'][idxs,:], Z0 = d['Z0'][idxs],
+                            mult = d['mult'][idxs]), Z = np.concatenate([d['Zlist'][j] for j in idxs]),
+                            lower = [0.1], upper = [50], covtype = "Matern5_2",
+                            known = dict(theta = model.theta, g = model.g,
+                                        beta0 = 0))
+            model_i.nu_hat = model.nu_hat
+            p_i = model_i.predict(d['X0'][i:i+1,:])
+            LOO_ref[i,:] = np.array([p_i['mean'], p_i['sd2']]).squeeze()
+    >>> print(np.ptp(LOO_ref[:,0] - LOO_p['mean']))
+    >>> print(np.ptp(LOO_ref[:,1] - LOO_p['sd2']))
+    >>> model.plot()
     '''
     if ids is None: ids = np.arange(model.X0.shape[0])
     
