10b6f2b8215b79e2ef2ebdad073a71e39999ba98
David O'Gara <david.ogara@wustl.edu>
2024-12-03 13:11:55 -0600

    make R libraries install to personal library

--- tests/install_R.py
+++ tests/install_R.py
@@ -2,6 +2,8 @@ from rpy2.robjects import r;
 
 if __name__ == "__main__":
     r("""
+      dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)  # create personal library
+      .libPaths(Sys.getenv("R_LIBS_USER"))  # add to the path
       deps <-c('hetGP','R.matlab','plgp')
       to_install <- deps[!(deps %in% installed.packages()[,"Package"])]
       if(length(to_install)) install.packages(to_install,repos='https://cloud.r-project.org')
